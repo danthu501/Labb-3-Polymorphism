243f81fda3d5021d227e813920da8c49906c2e66
danthu501 <91268795+danthu501@users.noreply.github.com>
2021-11-13 14:31:19 +0100

    Add project files.

---
+++ Labb 3 – Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Labb_3___Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cirkel C1 = new Cirkel();
+            Console.WriteLine("Area Cirkel: "+C1.Area());
+
+            Fyrkant F1 = new Fyrkant();
+            Console.WriteLine("Area Fyrkant: "+F1.Area());
+
+            Rektangel R1 = new Rektangel();
+            Console.WriteLine("Area Rektangel: "+R1.Area());
+        }
+    }
+}
---
+++ Labb 3 – Polymorphism/Teckning.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_3___Polymorphism
+{
+    public class Teckning
+    {
+        public virtual double Area()
+        {
+            int i = 1;
+            return i;
+        }
+    }
+    public class Rektangel : Teckning
+    {
+        public double _Width { get; set; }
+        public double _Height { get; set; }
+
+        public Rektangel()
+        {
+            _Width = 9.01;
+            _Height = 2;
+        }
+            
+        public  override double Area()
+        {
+            double Area = _Width * _Height;
+            return Area;
+        }
+
+    }
+    public class Fyrkant : Teckning
+    {
+        public double _length { get; set; }
+
+        public Fyrkant()
+        {
+            _length = 6;
+        }
+        public override double Area()
+        {
+            double Area = _length * _length;
+            return Area;
+        }
+
+    }
+    public class Cirkel : Teckning
+    {
+        public double _Radius { get; set; }
+        public double Pi { get; set; } = 3.141;
+
+        public Cirkel()
+        {
+            _Radius = 5;
+        }
+        public override double Area()
+        {
+            double Area = _Radius * _Radius* Pi;
+            return Area;
+        }
+    }
+}
